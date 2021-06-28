842b9052a45c71542af2984434eb411ea28a4e5a
Domi <domi@dev.null>
2021-06-28 03:44:52 +0200

    Improved redstone behavior and made villagers able to open double doors

--- src/main/java/re/domi/doors/mixin/DoorBlockMixin.java
+++ src/main/java/re/domi/doors/mixin/DoorBlockMixin.java
@@ -6,7 +6,9 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.enums.DoorHinge;
+import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.util.ActionResult;
@@ -15,10 +17,12 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -37,19 +41,17 @@ public class DoorBlockMixin extends Block
     @Inject(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;cycle(Lnet/minecraft/state/property/Property;)Ljava/lang/Object;"))
     public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir)
     {
-        if (ConnectedDoors.modPresent)
+        this.forConnectedDoor(state, world, pos, (neighborPos, neighborState, open) ->
         {
-            this.updateConnectedDoor(state, world, pos, (neighborPos, neighborState, open) ->
-                world.setBlockState(neighborPos, neighborState.with(OPEN, !open), Block.NOTIFY_LISTENERS | Block.REDRAW_ON_MAIN_THREAD));
-        }
-        else
-        {
-            this.updateConnectedDoor(state, world, pos, (neighborPos, neighborState, open) ->
-                {
-                    world.setBlockState(neighborPos, neighborState.with(OPEN, !open), Block.NOTIFY_LISTENERS | Block.REDRAW_ON_MAIN_THREAD);
-                    sendUsePacket(hand, hit, neighborPos);
-                });
-        }
+            world.setBlockState(neighborPos, neighborState.with(OPEN, !open), Block.NOTIFY_LISTENERS | Block.REDRAW_ON_MAIN_THREAD);
+
+            if (!ConnectedDoors.modPresent)
+            {
+                sendUsePacket(hand, hit, neighborPos);
+            }
+
+            return true;
+        });
     }
 
     @Inject(method = "neighborUpdate", locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
@@ -57,17 +59,31 @@ public class DoorBlockMixin extends Block
     {
         if (!world.isClient())
         {
-            this.updateConnectedDoor(state, world, pos, (neighborPos, neighborState, open) ->
-                world.setBlockState(neighborPos, neighborState.with(OPEN, receivingPower), Block.NOTIFY_LISTENERS));
+            this.forConnectedDoor(state, world, pos, (neighborPos, neighborState, open) ->
+                world.setBlockState(neighborPos, neighborState.with(OPEN, receivingPower).with(POWERED, receivingPower), Block.NOTIFY_LISTENERS));
         }
     }
 
+    @ModifyVariable(method = "neighborUpdate", ordinal = 1, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/DoorBlock;getDefaultState()Lnet/minecraft/block/BlockState;"))
+    public boolean neighborUpdateReceivingPower(boolean orig, BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify)
+    {
+        return orig || this.forConnectedDoor(state, world, pos, (neighborPos, neighborState, open) ->
+            world.isReceivingRedstonePower(neighborPos) || world.isReceivingRedstonePower(neighborPos.offset(neighborState.get(HALF) == DoubleBlockHalf.LOWER ? Direction.UP : Direction.DOWN)));
+    }
+
+    @Inject(method = "setOpen", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+    public void setOpen(@Nullable Entity entity, World world, BlockState state, BlockPos pos, boolean newOpen, CallbackInfo ci)
+    {
+        this.forConnectedDoor(state, world, pos, (neighborPos, neighborState, open) ->
+            world.setBlockState(neighborPos, neighborState.with(OPEN, newOpen), NOTIFY_LISTENERS));
+    }
+
     @Unique
-    private void updateConnectedDoor(BlockState state, World world, BlockPos pos, UpdateDoorCallback callback)
+    private boolean forConnectedDoor(BlockState state, World world, BlockPos pos, ForConnectedDoorFunc func)
     {
         Direction facing = state.get(FACING);
         DoorHinge hinge = state.get(HINGE);
-        boolean open = state.get(OPEN); // this value hasn't been updated yet
+        boolean open = state.get(OPEN);
 
         BlockPos neighborPos = pos.offset(hinge == DoorHinge.LEFT ? facing.rotateClockwise(Direction.Axis.Y) : facing.rotateCounterclockwise(Direction.Axis.Y));
         BlockState neighborState = world.getBlockState(neighborPos);
@@ -75,8 +91,10 @@ public class DoorBlockMixin extends Block
         if (neighborState.getBlock() == this && neighborState.get(FACING) == facing && neighborState.get(HINGE) != hinge
             && neighborState.get(HALF) == state.get(HALF) && neighborState.get(OPEN) == open)
         {
-            callback.run(neighborPos, neighborState, open);
+            return func.invoke(neighborPos, neighborState, open);
         }
+
+        return false;
     }
 
     @Unique
@@ -87,8 +105,8 @@ public class DoorBlockMixin extends Block
         MinecraftClient.getInstance().getNetworkHandler().sendPacket(new PlayerInteractBlockC2SPacket(hand, new BlockHitResult(hit.getPos(), hit.getSide(), pos, hit.isInsideBlock())));
     }
 
-    private interface UpdateDoorCallback
+    private interface ForConnectedDoorFunc
     {
-        void run(BlockPos neighborPos, BlockState neighborState, boolean open);
+        boolean invoke(BlockPos neighborPos, BlockState neighborState, boolean open);
     }
 }
